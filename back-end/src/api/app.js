5e5fcfae5dd746bcf413e80d477f647b641f8cae
Leonardo Prado <leonardo.rprado@gmail.com>
2023-03-30 15:22:37 +0100

    style: colocado ErrorHandler no app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./routes/router');
+const ErrorHandler = require('./middlewares/ErrorHandler');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
+app.use(ErrorHandler.handle);
 module.exports = app;
