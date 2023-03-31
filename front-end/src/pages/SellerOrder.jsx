a3e219aa372f8091b0435703de3a430d58d5ddfd
Leonardo Prado <leonardo.rprado@gmail.com>
2023-03-31 14:47:06 +0100

    feat(sellerOrder): adicionado orderCard

--- front-end/src/pages/SellerOrder.jsx
+++ front-end/src/pages/SellerOrder.jsx
@@ -1,7 +1,14 @@
 import Header from '../components/Header';
+import OrderCard from '../components/OrderCard';
+import '../styles/pages/sellerOrder.css';
 
 export default function SellerOrder() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <div className="c-seller-order">
+        <OrderCard />
+      </div>
+    </div>
   );
 }
