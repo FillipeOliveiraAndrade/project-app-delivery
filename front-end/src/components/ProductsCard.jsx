6f39e6bd0c742ee23ba3a884e647492184821d7e
fillipe <fillipe.trybe@gmail.com>
2023-04-04 03:33:11 -0300

    Requisito 15 incompleto

--- front-end/src/components/ProductsCard.jsx
+++ front-end/src/components/ProductsCard.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import Context from '../context/Context';
 
 function ProductsCard({ product }) {
+  const { productsInTheCart, setProductsInTheCart } = useContext(Context);
+
+  const [counter, setCounter] = useState(0);
+
   const customerProducts = 'customer_products__element';
   const cardPrice = 'card-price';
   const cardTitle = 'card-title';
+
+  const oneLess = -1;
+
+  useEffect(() => {
+    localStorage.setItem('shoppingCart', JSON.stringify(productsInTheCart));
+  }, [productsInTheCart]);
+
+  function removeProductInTheCart(productToBeRemoved) {
+    if (counter === 0) return;
+    setCounter((prevState) => prevState - 1);
+
+    const productIndex = productsInTheCart
+      .findIndex((item) => item.id === productToBeRemoved.id);
+
+    if (productIndex !== oneLess) {
+      const updatedCartItems = [...productsInTheCart];
+      updatedCartItems.splice(productIndex, 1);
+      setProductsInTheCart(updatedCartItems);
+    }
+  }
+
+  function addProductInTheCart(newProduct) {
+    setCounter((prevState) => prevState + 1);
+
+    setProductsInTheCart((prevState) => [newProduct, ...prevState]);
+  }
 
   return (
     <div>
@@ -31,14 +62,15 @@ function ProductsCard({ product }) {
       <button
         type="button"
         data-testid={ `customer_products__button-card-rm-item-${product.id}` }
-        // onClick={ () => removeProductInTheCart(product) }
+        onClick={ () => removeProductInTheCart(product) }
       >
         -
       </button>
 
       <input
         data-testid={ `customer_products__input-card-quantity-${product.id}` }
-        value="0"
+        min={ 0 }
+        value={ counter }
       />
 
       <button
--- front-end/src/components/ShoppingCart.jsx
+++ front-end/src/components/ShoppingCart.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import context from '../context/Context';
 
 function ShoppingCart() {
+  const history = useHistory();
+
+  const { productsInTheCart } = useContext(context);
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const cartItems = JSON.parse(localStorage.getItem('shoppingCart'));
+  useEffect(() => {
+    if (cartItems) {
+      const cartTotalPrice = productsInTheCart.reduce(
+        (total, item) => total + Number(item.price || item.unitPrice),
+        0,
+      );
+      setTotalPrice(cartTotalPrice);
+    }
+  }, [productsInTheCart]);
+
   return (
-    <div>
-      Ver carrinho de compras
-      {' '}
-      0
-    </div>
+    <button
+      data-testid="customer_products__button-cart"
+      type="button"
+      onClick={ () => history.push('/customer/checkout') }
+    >
+      <p data-testid="customer_products__checkout-bottom-value">
+        { (totalPrice.toFixed(2)).toString().replace('.', ',') }
+      </p>
+    </button>
   );
 }
 
