73acd3a7b3390b088d9a65dcb5d5cb1542adabde
Arthur Araujo <technoasti@gmail.com>
2023-04-05 07:57:46 -0300

    Copiando os arquivos do package-lock.json

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -84,11 +84,10 @@ export default function Checkout() {
           <tbody>
             {
               cart
-                && (
+                &&
                   cart.items.map((order, index) => (
                     <TableItens order={ order } key={ order.id } index={ index } />
                   ))
-                )
             }
           </tbody>
         </table>
